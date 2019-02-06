c7cf1d9d10652a75eb416d6e84de9333fe9ed1a7
tehnerd <tehnerd@tehnerd.com>
2019-02-06 10:01:53 -0800

    adding explicit healthchecks interface configuration into grpc example

--- example_grpc/katran_server.cpp
+++ example_grpc/katran_server.cpp
@@ -38,6 +38,7 @@ using grpc::ServerBuilder;
 
 DEFINE_int32(port, 10889, "Service port");
 DEFINE_string(intf, "eth0", "main interface");
+DEFINE_string(hc_intf, "", "interface for healthchecking");
 DEFINE_string(ipip_intf, "ipip0", "ipip (v4) encap interface");
 DEFINE_string(ipip6_intf, "ipip60", "ip(6)ip6 (v6) encap interface");
 DEFINE_string(balancer_prog, "./balancer_kern.o", "path to balancer bpf prog");
@@ -127,6 +128,7 @@ int main(int argc, char** argv) {
   config.LruSize = static_cast<uint64_t>(FLAGS_lru_size);
   config.forwardingCores = forwardingCores;
   config.numaNodes = numaNodes;
+  config.hcInterface = FLAGS_hc_intf;
 
   auto evb = std::make_shared<folly::EventBase>();
   std::thread t1([evb](){
