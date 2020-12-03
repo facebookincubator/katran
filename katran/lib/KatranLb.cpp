2a62435c7423c9a20d23e07f765da9089995d094
Jalal Sajadi <jalal.sajadi@gmail.com>
2020-12-03 05:34:27 +0330

    cleanup codes

--- katran/lib/KatranLb.cpp
+++ katran/lib/KatranLb.cpp
@@ -884,11 +884,42 @@ bool KatranLb::modifyLocalMarkForReal(const ModifyAction action, const NewReal& 
     vip.port,
     vip.proto);
 
+  auto vip_iter = vips_.find(vip);
+  if (vip_iter == vips_.end()) {
+    LOG(INFO) << "trying to change non existing vip";
+    return false;
+  }
+  auto vip_addr = IpHelpers::parseAddrToBe(vip.address);
+  vip_definition vip_def = {};
+  if ((vip_addr.flags & V6DADDR) > 0) {
+    std::memcpy(vip_def.vipv6, vip_addr.v6daddr, 16);
+  } else {
+    vip_def.vip = vip_addr.daddr;
+  }
+  vip_def.port = folly::Endian::big(vip.port);
+  vip_def.proto = vip.proto;
+  vip_meta meta;
+  meta.vip_num = vip_iter->second.getVipNum();
+  meta.flags = vip_iter->second.getVipFlags();
+  if (action == ModifyAction::ADD) {
+    meta.flags |= kLocalVip;
+  } else {
+    meta.flags &= 0xffffffff ^ kLocalVip;
+  }
+  res = bpfAdapter_.bpfUpdateMap(
+    bpfAdapter_.getMapFdByName("vip_map"), &vip_def, &meta);
+  if (res != 0) {
+    LOG(INFO) << "can't modify vip_map, error: "
+              << folly::errnoStr(errno);
+    lbStats_.bpfFailedCalls++;
+    return false;
+  }
   auto real_iter = reals_.find(raddr);
   if (real_iter == reals_.end()) {
     LOG(INFO) << "trying to modify local mark non-existing real";
-    continue;
+    return false;
   }
+  auto cur_reals = vip_iter->second.getReals();
   auto num = real_iter->second.num;
   if (std::find(
     cur_reals.begin(), cur_reals.end(), num) ==
@@ -896,7 +927,7 @@ bool KatranLb::modifyLocalMarkForReal(const ModifyAction action, const NewReal& 
     // this real doesn't belong to this vip
     LOG(INFO) << folly::sformat(
       "trying to modify non-existing real for the VIP: {}", vip.address);
-    continue;
+    return false;
   }
   auto real_addr = IpHelpers::parseAddrToBe(raddr);
   if (action == ModifyAction::ADD){
@@ -908,38 +939,6 @@ bool KatranLb::modifyLocalMarkForReal(const ModifyAction action, const NewReal& 
     bpfAdapter_.getMapFdByName("reals"), &num, &real_addr);
   if (res != 0) {
     LOG(INFO) << "can't modify real, error: " << folly::errnoStr(errno);
-    lbStats_.bpfFailedCalls++;
-    return false;
-  }
-
-  auto vip_iter = vips_.find(vip);
-  if (vip_iter == vips_.end()) {
-    LOG(INFO) << "trying to change non existing vip";
-    return false;
-  }
-  vip_meta meta;
-  meta.vip_num = vip_iter->second.getVipNum();
-  meta.flags = vip_iter->second.getVipFlags();
-
-  auto vip_addr = IpHelpers::parseAddrToBe(vip.address);
-  vip_definition vip_def = {};
-  if ((vip_addr.flags & V6DADDR) > 0) {
-    std::memcpy(vip_def.vipv6, vip_addr.v6daddr, 16);
-  } else {
-    vip_def.vip = vip_addr.daddr;
-  }
-  vip_def.port = folly::Endian::big(vip.port);
-  vip_def.proto = vip.proto;
-  if (action == ModifyAction::ADD) {
-    meta.flags |= kLocalVip;
-  } else {
-    meta.flags &= 0xffffffff ^ kLocalVip;
-  }
-  res = bpfAdapter_.bpfUpdateMap(
-    bpfAdapter_.getMapFdByName("vip_map"), &vip_def, &meta);
-  if (res != 0) {
-    LOG(INFO) << "can't modify vip_map, error: "
-              << folly::errnoStr(errno);
     lbStats_.bpfFailedCalls++;
     return false;
   }
