6fa82bb77efeebedfc879a573a6d9a892fc44e2a
Jalal Sajadi <jalal.sajadi@gmail.com>
2020-12-08 15:31:09 +0330

    move packet encap after xdp_pass for local traffic

--- katran/lib/bpf/balancer_kern.c
+++ katran/lib/bpf/balancer_kern.c
@@ -604,15 +604,6 @@ static inline int process_packet(void *data, __u64 off, void *data_end,
     return XDP_DROP;
   }
 
-  if (dst->flags & F_IPV6) {
-    if(!PCKT_ENCAP_V6(xdp, cval, is_ipv6, &pckt, dst, pkt_bytes)) {
-      return XDP_DROP;
-    }
-  } else {
-    if(!PCKT_ENCAP_V4(xdp, cval, &pckt, dst, pkt_bytes)) {
-      return XDP_DROP;
-    }
-  }
   vip_num = vip_info->vip_num;
   data_stats = bpf_map_lookup_elem(&stats, &vip_num);
   if (!data_stats) {
@@ -633,6 +624,15 @@ static inline int process_packet(void *data, __u64 off, void *data_end,
     return XDP_PASS;
   }
 #endif
+  if (dst->flags & F_IPV6) {
+    if(!PCKT_ENCAP_V6(xdp, cval, is_ipv6, &pckt, dst, pkt_bytes)) {
+      return XDP_DROP;
+    }
+  } else {
+    if(!PCKT_ENCAP_V4(xdp, cval, &pckt, dst, pkt_bytes)) {
+      return XDP_DROP;
+    }
+  }
 
   return XDP_TX;
 }
