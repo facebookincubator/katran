facebookincubator/katran

commit f6e5cbceda8d4cdd87dcc2f5c4e420f107276172

return xdp_pass for local reals (#107)
Summary:
This PR is to support passing traffic when the destination server is the same as the Katran server.
It improves performance when we use a single server as a load-balancer and also as a production server.

Pull Request resolved: https://github.com/facebookincubator/katran/pull/107

Reviewed By: sharmafb

Differential Revision: D25348174

Pulled By: udippant

fbshipit-source-id: e7067e8af5087d727f49bd6e1a892044efe9a257

example_grpc/goclient/src/katranc/katranc/katranc.go
@@ -36,6 +36,9 @@ const (
 	NO_LRU      = 2
 	QUIC_VIP    = 4
 	DPORT_HASH  = 8
+	LOCAL_VIP   = 32
+
+	LOCAL_REAL  = 2
 )
 
 const (
@@ -45,11 +48,15 @@ const (
 )
 
 var (
-	flagTranslationTable = map[string]int64{
+	vipFlagTranslationTable = map[string]int64{
 		"NO_SPORT":   NO_SPORT,
 		"NO_LRU":     NO_LRU,
 		"QUIC_VIP":   QUIC_VIP,
 		"DPORT_HASH": DPORT_HASH,
+		"LOCAL_VIP":  LOCAL_VIP,
+	}
+	realFlagTranslationTable = map[string]int32{
+		"LOCAL_REAL":  LOCAL_REAL,
 	}
 )
 
@@ -118,10 +125,11 @@ func parseToVip(addr string, proto int) lb_katran.Vip {
 	return vip
 }
 
-func parseToReal(addr string, weight int64) lb_katran.Real {
+func parseToReal(addr string, weight int64, flags int32) lb_katran.Real {
 	var real lb_katran.Real
 	real.Address = addr
 	real.Weight = int32(weight)
+	real.Flags = flags
 	return real
 }
 
@@ -145,7 +153,7 @@ func (kc *KatranClient) AddOrModifyService(
 	var flags int64
 	var exists bool
 	if flagsString != "" {
-		if flags, exists = flagTranslationTable[flagsString]; !exists {
+		if flags, exists = vipFlagTranslationTable[flagsString]; !exists {
 			log.Printf("unrecognized flag: %v\n", flagsString)
 			return
 		}
@@ -161,6 +169,18 @@ func (kc *KatranClient) DelService(addr string, proto int) {
 	log.Printf("Deleting service: %v %v\n", addr, proto)
 	vip := parseToVip(addr, proto)
 	kc.UpdateService(vip, 0, DEL_VIP, false)
+}
+
+func (kc *KatranClient) UpdateReal(addr string, flags int32, setFlags bool) {
+	var rMeta lb_katran.RealMeta
+	rMeta.Address = addr
+	rMeta.Flags = flags
+	rMeta.SetFlag = setFlags
+	ok, err = kc.client.ModifyReal(context.Background(), &rMeta)
+	checkError(err)
+	if ok.Success {
+		log.Printf("Real modified\n")
+	}
 }
 
 func (kc *KatranClient) UpdateService(
@@ -191,9 +211,17 @@ func (kc *KatranClient) UpdateService(
 }
 
 func (kc *KatranClient) UpdateServerForVip(
-	vipAddr string, proto int, realAddr string, weight int64, delete bool) {
+	vipAddr string, proto int, realAddr string, weight int64, realFlags string, delete bool) {
 	vip := parseToVip(vipAddr, proto)
-	real := parseToReal(realAddr, weight)
+	var flags int32
+	var exists bool
+	if realFlags != "" {
+		if flags, exists = realFlagTranslationTable[realFlags]; !exists {
+			log.Printf("unrecognized flag: %v\n", realFlags)
+			return
+		}
+	}
+	real := parseToReal(realAddr, weight, flags)
 	var action lb_katran.Action
 	if delete {
 		action = lb_katran.Action_DEL
@@ -264,7 +292,7 @@ func (kc *KatranClient) GetFlags(vip *lb_katran.Vip) uint64 {
 	return flags.Flags
 }
 
-func parseFlags(flags uint64) string {
+func parseVipFlags(flags uint64) string {
 	flags_str := ""
 	if flags&uint64(NO_SPORT) > 0 {
 		flags_str += " NO_SPORT "
@@ -277,6 +305,17 @@ func parseFlags(flags uint64) string {
 	}
 	if flags&uint64(DPORT_HASH) > 0 {
 		flags_str += " DPORT_HASH "
+	}
+	if flags&uint64(LOCAL_VIP) > 0 {
+		flags_str += " LOCAL_VIP "
+	}
+	return flags_str
+}
+
+func parseRealFlags(flags uint32) string {
+	flags_str := ""
+	if flags&uint32(LOCAL_REAL) > 0 {
+		flags_str += " LOCAL_REAL "
 	}
 	return flags_str
 }
@@ -293,12 +332,12 @@ func (kc *KatranClient) ListVipAndReals(vip *lb_katran.Vip) {
 		vip.Address,
 		vip.Port,
 		proto)
-	flags := kc.GetFlags(vip)
-	fmt.Printf("Vip's flags: %v\n", parseFlags(flags))
+	flags := kc.GetVipFlags(vip)
+	fmt.Printf("Vip's flags: %v\n", parseVipFlags(flags))
 	for _, real := range reals.Reals {
-		fmt.Printf("%-20v weight: %v\n",
+		fmt.Printf("%-20v weight: %v flags: %v\n",
 			" ->"+real.Address,
-			real.Weight)
+			real.Weight, parseRealFlags(real.Flags))
 	}
 }
 

example/KatranSimpleServiceHandler.cpp
@@ -39,6 +39,7 @@ using Guard = std::lock_guard<std::mutex>;
   ::katran::NewReal nr;
   nr.address = real.address;
   nr.weight = real.weight;
+  nr.flags = real.flags;
   return nr;
 }
 
@@ -111,6 +112,12 @@ bool KatranSimpleServiceHandler::modifyVip(
   auto vk = translateVipObject(vipMeta->vip);
   Guard lock(giant_);
   return lb_.modifyVip(vk, vipMeta->flags, vipMeta->setFlag);
+}
+
+bool KatranSimpleServiceHandler::modifyReal(
+  std::unique_ptr<::lb::katran::RealMeta> realMeta) {
+  Guard lock(giant_);
+  return lb_.modifyReal(realMeta->address, realMeta->flags, realMeta->setFlag);
 }
 
 int64_t KatranSimpleServiceHandler::getVipFlags(
@@ -201,6 +208,7 @@ void KatranSimpleServiceHandler::getRealsForVip(
   for (auto& real : reals) {
     r.address = real.address;
     r.weight = real.weight;
+    r.flags = real.flags;
     _return.push_back(r);
   }
   return;

example/KatranSimpleServiceHandler.h
@@ -48,6 +48,8 @@ class KatranSimpleServiceHandler : virtual public KatranServiceSvIf {
 
   bool modifyVip(std::unique_ptr<::lb::katran::VipMeta> vipMeta) override;
 
+  bool modifyReal(std::unique_ptr<::lb::katran::RealMeta> realMeta) override;
+
   int64_t getVipFlags(std::unique_ptr<::lb::katran::Vip> vip) override;
 
   bool addRealForVip(

example/client/KatranSimpleClient.cpp
@@ -43,10 +43,15 @@ constexpr uint64_t NO_SPORT = 1;
 constexpr uint64_t NO_LRU = 2;
 constexpr uint64_t QUIC_VIP = 4;
 constexpr uint64_t DPORT_HASH = 8;
+constexpr uint64_t LOCAL_VIP = 32;
+constexpr uint32_t LOCAL_REAL = 2;
 
-const std::map<std::string, uint64_t> flagTranslationTable = {
+const std::map<std::string, uint64_t> vipFlagTranslationTable = {
     {"", DEFAULT_FLAG},     {"NO_SPORT", NO_SPORT},     {"NO_LRU", NO_LRU},
-    {"QUIC_VIP", QUIC_VIP}, {"DPORT_HASH", DPORT_HASH},
+    {"QUIC_VIP", QUIC_VIP}, {"DPORT_HASH", DPORT_HASH}, {"LOCAL_VIP", LOCAL_VIP},
+};
+const std::map<std::string, uint32_t> realFlagTranslationTable = {
+  {"LOCAL_REAL", LOCAL_REAL},
 };
 }; // namespace
 
@@ -80,8 +85,8 @@ void KatranSimpleClient::addOrModifyService(const std::string &address,
                                             bool modify, bool setFlags) {
   LOG(INFO) << folly::sformat("Adding service: {} {}", address, proto);
   auto vip = parseToVip(address, proto);
-  const auto &it = flagTranslationTable.find(flags);
-  if (it == flagTranslationTable.cend()) {
+  const auto &it = vipFlagTranslationTable.find(flags);
+  if (it == vipFlagTranslationTable.cend()) {
     LOG(ERROR) << folly::sformat("ERROR: unrecognized flag: {}", flags);
     return;
   }
@@ -114,12 +119,33 @@ void KatranSimpleClient::delService(const std::string &address, int proto) {
   }
 }
 
+void KatranSimpleClient::updateReal(const std::string &address, uint32_t flags, bool setFlags) {
+  LOG(INFO) << folly::sformat("Updating real: {} {}", address, proto);
+  RealMeta realMeta;
+  realMeta.address = address;
+  realMeta.flags = flags;
+  realMeta.setFlags = setFlags;
+
+  if (client_->sync_modifyReal(std::move(realMeta))) {
+    LOG(INFO) << "Real updated";
+  } else {
+    LOG(ERROR) << "ERROR: Real not updated";
+  }
+}
+
 void KatranSimpleClient::updateServerForVip(const std::string &vipAddr,
                                             int proto,
                                             const std::string &realAddr,
-                                            uint64_t weight, bool del) {
+                                            uint64_t weight,
+                                            const std::string &flags,
+                                            bool del) {
   auto vip = parseToVip(vipAddr, proto);
-  auto real = parseToReal(realAddr, weight);
+  const auto& it = realFlagTranslationTable.find(flags);
+  if (it == realFlagTranslationTable.cend()) {
+    LOG(ERROR) << folly::sformat("ERROR: unrecognized flag: {}", flags);
+    return;
+  }
+  auto real = parseToReal(realAddr, weight, it->second);
   Action action;
   if (del) {
     action = Action::DEL;
@@ -176,7 +202,7 @@ uint64_t KatranSimpleClient::getFlags(const Vip &vip) {
   return client_->sync_getVipFlags(vip);
 }
 
-std::string KatranSimpleClient::parseFlags(uint64_t flags) {
+std::string KatranSimpleClient::parseVipFlags(uint64_t flags) {
   std::string flagsStr = "";
   if ((flags & NO_SPORT) > 0) {
     flagsStr += " NO_SPORT ";
@@ -189,6 +215,17 @@ std::string KatranSimpleClient::parseFlags(uint64_t flags) {
   }
   if ((flags & DPORT_HASH) > 0) {
     flagsStr += " DPORT_HASH ";
+  }
+  if ((flags & LOCAL_VIP) > 0) {
+    flagsStr += " LOCAL_VIP ";
+  }
+  return flagsStr;
+}
+
+std::string KatranSimpleClient::parseRealFlags(uint32_t flags) {
+  std::string flagsStr = "";
+  if ((flags & LOCAL_REAL) > 0) {
+    flagsStr += " LOCAL_REAL ";
   }
   return flagsStr;
 }
@@ -212,10 +249,10 @@ void KatranSimpleClient::listVipAndReals(const Vip &vip) {
   LOG(INFO) << folly::sformat("VIP: {:<20} Port: {:06d}, Protocol: {}",
                               vip.address, vip.port, proto);
   uint64_t flags = getFlags(vip);
-  LOG(INFO) << folly::sformat("Vip's flags: {}", parseFlags(flags));
+  LOG(INFO) << folly::sformat("Vip's flags: {}", parseVipFlags(flags));
   for (auto real : reals) {
-    LOG(INFO) << folly::sformat("-> {:<20} weight {}", real.address,
-                                real.weight);
+    LOG(INFO) << folly::sformat("-> {:<20} weight {} flags {}", real.address,
+                                real.weight, parseRealFlags(real.flags));
   }
 }
 
@@ -438,10 +475,11 @@ Vip KatranSimpleClient::parseToVip(const std::string &address,
 }
 
 Real KatranSimpleClient::parseToReal(const std::string &address,
-                                     uint32_t weight) {
+                                     uint32_t weight, uint32_t flags) {
   Real real;
   real.address = address;
   real.weight = weight;
+  real.flags = flags;
   return real;
 }
 

example/client/KatranSimpleClient.h
@@ -53,8 +53,11 @@ public:
 
   void updateService(Vip &vip, uint64_t flags, Action action, bool setFlags);
 
+ void updateReal(const std::string &address, uint32_t flags, bool setFlags);
+
   void updateServerForVip(const std::string &vipAddr, int proto,
                           const std::string &realAddr, uint64_t weight,
+                          const std::string &flags,
                           bool del);
 
   void modifyQuicMappings(const std::string &mapping, bool del);
@@ -65,7 +68,7 @@ public:
 
   Reals getRealsForVip(const Vip &vip);
 
-  uint64_t getFlags(const Vip &Vip);
+  uint64_t getFlags(const Vip &vip);
 
   void listVipAndReals(const Vip &vip);
 
@@ -92,11 +95,13 @@ public:
 private:
   Vip parseToVip(const std::string &address, uint32_t protocol);
 
-  Real parseToReal(const std::string &address, uint32_t weight);
+  Real parseToReal(const std::string &address, uint32_t weight, uint32_t flags);
 
   QuicReal parseToQuicReal(const std::string &mapping);
 
-  std::string parseFlags(uint64_t flags);
+  std::string parseVipFlags(uint64_t flags);
+
+  std::string parseRealFlags(uint32_t flags);
 
   // factory method to create KatranServiceClient instance
   std::unique_ptr<KatranServiceAsyncClient>

example/client/Main.cpp
@@ -44,8 +44,12 @@ DEFINE_bool(icmp, false, "Show ICMP \"packet too big\"  related stats");
 DEFINE_bool(l, false, "List configured services");
 DEFINE_bool(C, false, "Clear all configs");
 DEFINE_string(
-    f, "",
-    "change flags. Possible values: NO_SPORT, NO_LRU, QUIC_VIP, DPORT_HASH");
+    vf, "",
+    "change vip flags. Possible values: "
+    "NO_SPORT, NO_LRU, QUIC_VIP, DPORT_HASH, LOCAL_VIP");
+DEFINE_string(
+    rf, "",
+    "change real flags. Possible values: LOCAL_REAL");
 DEFINE_bool(unset, false, "Unset specified flags");
 DEFINE_string(new_hc, "", "Address of new backend to healthcheck");
 DEFINE_uint64(somark, 0, "Socket mark to specified backend");
@@ -84,7 +88,8 @@ int main(int argc, char **argv) {
   const bool showIcmpStatsFlag = FLAGS_icmp;
   const bool listServicesFlag = FLAGS_l;
   const bool clearAllFlag = FLAGS_C;
-  const std::string changeFlags = FLAGS_f;
+  const std::string vipChangeFlags = FLAGS_vf;
+  const std::string realChangeFlags = FLAGS_rf;
 
   FLAGS_logtostderr = 1;
   std::string service{""};
@@ -103,19 +108,19 @@ int main(int argc, char **argv) {
   } else if (FLAGS_list_mac) {
     client.getMac();
   } else if (addServiceFlag) {
-    client.addOrModifyService(service, changeFlags, proto, false, true);
+    client.addOrModifyService(service, vipChangeFlags, proto, false, true);
   } else if (listServicesFlag) {
     client.list("", proto);
   } else if (delServiceFlag) {
     client.delService(service, proto);
   } else if (editServiceFlag) {
-    client.addOrModifyService(service, changeFlags, proto, true, !FLAGS_unset);
+    client.addOrModifyService(service, vipChangeFlags, proto, true, !FLAGS_unset);
   } else if (addServerFlag || editServerFlag) {
     client.updateServerForVip(service, proto, realServerFlag, realWeightFlag,
-                              false);
+                              realChangeFlags, false);
   } else if (delServerFlag) {
     client.updateServerForVip(service, proto, realServerFlag, realWeightFlag,
-                              true);
+                              realChangeFlags, true);
   } else if (FLAGS_del_qm) {
     if (FLAGS_quic_mapping == "") {
       LOG(FATAL) << "quic_mapping is not specified.";

example/katran.thrift
@@ -39,9 +39,19 @@ struct VipMeta {
   3: optional bool setFlag = 1,
 }
 
+struct RealMeta {
+  1: string address,
+  2: i32 flags,
+  /*
+   * setFlag controls if we setting this flags or removing it from the Real
+   */
+  3: optional bool setFlag = 1,
+}
+
 struct Real {
   1: string address,
   2: i32 weight,
+  3: i32 flags,
 }
 
 struct QuicReal {
@@ -82,6 +92,8 @@ service KatranService {
   list<Vip> getAllVips();
 
   bool modifyVip (1: VipMeta vipMeta);
+
+  bool modifyReal (1: RealMeta realMeta);
 
   i64 getVipFlags(1: Vip vip);
 

example_grpc/KatranGrpcService.cpp
@@ -43,6 +43,7 @@ using Guard = std::lock_guard<std::mutex>;
   ::katran::NewReal nr;
   nr.address = real.address();
   nr.weight = real.weight();
+  nr.flags = real.flags();
   return nr;
 }
 
@@ -138,6 +139,15 @@ Status KatranGrpcService::modifyVip(ServerContext *context,
   auto vk = translateVipObject(request->vip());
   Guard lock(giant_);
   auto res = lb_.modifyVip(vk, request->flags(), request->setflag());
+  response->set_success(res);
+  return returnStatus(res);
+}
+
+Status KatranGrpcService::modifyReal(ServerContext *context,
+                                    const RealMeta *request, Bool *response) {
+
+  Guard lock(giant_);
+  auto res = lb_.modifyReal(request->address(), request->flags(), request->setflag());
   response->set_success(res);
   return returnStatus(res);
 }
@@ -240,6 +250,7 @@ Status KatranGrpcService::getRealsForVip(ServerContext *context,
   for (auto &real : reals) {
     r.set_address(real.address);
     r.set_weight(real.weight);
+    r.set_flags(real.flags);
     auto rr = response->add_reals();
     *rr = r;
   }

commit c8abe667753ca682ee1b5cbb1b7a4332224c56a6

Fix issues in the goclient
Summary:
Commit
https://github.com/facebookincubator/katran/commit/f6e5cbceda8d4cdd87dcc2f5c4e420f107276172
introduced few bugs in this client. This diff addresses that

Reviewed By: bschlinker

Differential Revision: D25783814

fbshipit-source-id: 18bb9c86a55e6625a6773604947c24d219c9aebd

example_grpc/goclient/src/katranc/katranc/katranc.go
@@ -176,7 +176,7 @@ func (kc *KatranClient) UpdateReal(addr string, flags int32, setFlags bool) {
 	rMeta.Address = addr
 	rMeta.Flags = flags
 	rMeta.SetFlag = setFlags
-	ok, err = kc.client.ModifyReal(context.Background(), &rMeta)
+	ok, err := kc.client.ModifyReal(context.Background(), &rMeta)
 	checkError(err)
 	if ok.Success {
 		log.Printf("Real modified\n")
@@ -286,7 +286,7 @@ func (kc *KatranClient) GetRealsForVip(vip *lb_katran.Vip) lb_katran.Reals {
 	return *reals
 }
 
-func (kc *KatranClient) GetFlags(vip *lb_katran.Vip) uint64 {
+func (kc *KatranClient) GetVipFlags(vip *lb_katran.Vip) uint64 {
 	flags, err := kc.client.GetVipFlags(context.Background(), vip)
 	checkError(err)
 	return flags.Flags
@@ -312,9 +312,12 @@ func parseVipFlags(flags uint64) string {
 	return flags_str
 }
 
-func parseRealFlags(flags uint32) string {
+func parseRealFlags(flags int32) string {
+	if flags < 0 {
+		log.Fatalf("invalid real flags passed: %v\n", flags)
+	}
 	flags_str := ""
-	if flags&uint32(LOCAL_REAL) > 0 {
+	if flags&LOCAL_REAL > 0 {
 		flags_str += " LOCAL_REAL "
 	}
 	return flags_str