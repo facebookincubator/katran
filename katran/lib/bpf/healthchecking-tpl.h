52f66cbf93e6082a5ee6fe061818aa8b34e5ca2b
Lalit Gupta <lalitgupta@meta.com>
2022-11-08 17:56:10 -0800

    library for encapsulating packets
    Summary: A simple library for encapsulating packet
    
    Reviewed By: sharmafb
    
    Differential Revision: D40402480
    
    fbshipit-source-id: fa2e489943e30cdbeb43fa9252463be21968b364

---
+++ katran/lib/bpf/healthchecking-tpl.h
@@ -0,0 +1,8 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+namespace katran {
+unsigned char kHealthcheckProgBuffer[] = {__HexdumpPlaceholder__};
+
+} // namespace katran
