e438e0243aad6d5c9c85d923c79fbf0df97e7005
Andrii Vasylevskyi <avas@meta.com>
2023-07-14 16:19:24 -0700

    TPR Skeleton - Extra utils from BpfAdapter
    Summary: Preparation for proper BPF skeleton usage in TPR. Extracted utility methods from `TprBpfAdatper` that are not provided but skeleton.
    
    Reviewed By: frankfeir
    
    Differential Revision: D47442733
    
    fbshipit-source-id: c01357d3fa25ed4b165214003709b83b7dd97d1c

---
+++ katran/tpr/bpf_util/BpfUtil.cpp
@@ -0,0 +1,112 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include <katran/tpr/bpf_util/BpfUtil.h>
+
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <folly/ScopeGuard.h>
+#include <folly/portability/SysResource.h>
+#include <folly/portability/Unistd.h>
+
+#include <errno.h>
+#include <glog/logging.h>
+
+extern "C" {
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+}
+
+namespace katran_tpr::BpfUtil {
+
+#if False
+static int libbpf_print_fn(
+    enum libbpf_print_level level,
+    const char* format,
+    va_list args) {
+  return vfprintf(stderr, format, args);
+}
+#endif
+
+SystemMaybe<folly::Unit> init() noexcept {
+#if False // Enable to get libbpf internal logs
+  libbpf_set_print(libbpf_print_fn);
+#endif
+
+  VLOG(1) << "Enabled libbpf strict mode";
+  libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+
+  return setRLimit();
+}
+
+SystemMaybe<folly::Unit> setRLimit() noexcept {
+  struct rlimit lck_mem = {};
+  lck_mem.rlim_cur = RLIM_INFINITY;
+  lck_mem.rlim_max = RLIM_INFINITY;
+  if (setrlimit(RLIMIT_MEMLOCK, &lck_mem)) {
+    int savedErrno = errno;
+    return SYSTEM_ERROR(
+        savedErrno,
+        "Error while setting limit for locked memory, error:",
+        folly::errnoStr(savedErrno));
+  }
+  return noSystemError();
+}
+
+int getDirFd(const std::string& path) noexcept {
+  return ::open(path.c_str(), O_DIRECTORY, O_RDONLY);
+}
+
+SystemMaybe<folly::Unit> attachCgroupProg(
+    int progFd,
+    const std::string& cgroup,
+    unsigned int flags) noexcept {
+  auto targetFd = getDirFd(cgroup);
+  if (targetFd < 0) {
+    return SYSTEM_ERROR(
+        EINVAL, fmt::format("Invalid target cgroup dir: {}", cgroup));
+  }
+  SCOPE_EXIT {
+    ::close(targetFd);
+  };
+  if (bpf_prog_attach(progFd, targetFd, BPF_CGROUP_SOCK_OPS, flags)) {
+    int savedErrno = errno;
+    return SYSTEM_ERROR(
+        savedErrno,
+        fmt::format(
+            "Error while attaching bpf prog fd: {} to cgroup: {}, error {} ",
+            progFd,
+            cgroup,
+            folly::errnoStr(savedErrno)));
+  }
+  LOG(INFO) << "Successfully attached bpf prog at cgroup: " << cgroup;
+  return noSystemError();
+}
+
+SystemMaybe<folly::Unit> detachCgroupProg(
+    int progFd,
+    const std::string& cgroup) noexcept {
+  auto targetFd = getDirFd(cgroup);
+  if (targetFd < 0) {
+    return SYSTEM_ERROR(
+        EINVAL, fmt::format("Invalid target cgroup dir: {}", cgroup));
+  }
+  SCOPE_EXIT {
+    ::close(targetFd);
+  };
+  if (bpf_prog_detach2(progFd, targetFd, BPF_CGROUP_SOCK_OPS)) {
+    int savedErrno = errno;
+    return SYSTEM_ERROR(
+        savedErrno,
+        fmt::format(
+            "Error while detaching bpf prog fd: {} to cgroup: {}, error {} ",
+            progFd,
+            cgroup,
+            folly::errnoStr(savedErrno)));
+  }
+  VLOG(2) << fmt::format(
+      "BPF program successfully detached from cgroup: {}", cgroup);
+  return noSystemError();
+}
+
+} // namespace katran_tpr::BpfUtil
---
+++ katran/tpr/bpf_util/BpfUtil.h
@@ -0,0 +1,48 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#pragma once
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <folly/String.h>
+
+#include <katran/tpr/bpf_util/SystemError.h>
+
+namespace katran_tpr::BpfUtil {
+
+constexpr int kNoFlags = 0;
+
+SystemMaybe<folly::Unit> init() noexcept;
+
+SystemMaybe<folly::Unit> setRLimit() noexcept;
+
+int getDirFd(const std::string& path) noexcept;
+
+SystemMaybe<folly::Unit> attachCgroupProg(
+    int progFd,
+    const std::string& cgroup,
+    unsigned int flags) noexcept;
+
+SystemMaybe<folly::Unit> detachCgroupProg(
+    int progFd,
+    const std::string& cgroup) noexcept;
+
+template <class K, class V>
+SystemMaybe<folly::Unit>
+updateMapElement(int mapFd, const K& key, const V& value) noexcept {
+  if (mapFd < 0) {
+    return SYSTEM_ERROR(EINVAL, fmt::format("Invalid map-fd given: {}", mapFd));
+  }
+  if (::bpf_map_update_elem(mapFd, &key, &value, kNoFlags)) {
+    int savedErrno = errno;
+    return SYSTEM_ERROR(
+        savedErrno,
+        fmt::format(
+            "Error while updating map: {}, error: {}",
+            mapFd,
+            folly::errnoStr(savedErrno)));
+  }
+  return noSystemError();
+};
+
+} // namespace katran_tpr::BpfUtil
