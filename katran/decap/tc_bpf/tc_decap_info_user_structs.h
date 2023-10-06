22af5a94032185a074c24853d98935ef8ba995f1
Lalit Gupta <lalitgupta@meta.com>
2023-10-06 14:31:40 -0700

    SynE2E+Shiv - logic to send shiv info in response header
    Summary: per title
    
    Reviewed By: avasylev
    
    Differential Revision: D48928262
    
    fbshipit-source-id: db1e46f026b7d4a173bf600228a32db2a1ce1f47

---
+++ katran/decap/tc_bpf/tc_decap_info_user_structs.h
@@ -0,0 +1,36 @@
+/* Copyright (C) 2019-present, Facebook, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __DECAP_INFO_USER_STRUCTS_H
+#define __DECAP_INFO_USER_STRUCTS_H
+
+struct flow_key {
+  union {
+    uint32_t src;
+    uint32_t srcv6[4];
+  };
+  union {
+    uint32_t dst;
+    uint32_t dstv6[4];
+  };
+  union {
+    uint32_t ports;
+    uint16_t port16[2];
+  };
+  uint8_t proto;
+};
+
+#endif // of __DECAP_INFO_USER_STRUCTS
