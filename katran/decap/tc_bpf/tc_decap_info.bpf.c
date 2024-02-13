e3917dd866a4f81eca71da1344b6077ab0c64afb
Lalit Gupta <lalitgupta@meta.com>
2024-02-13 15:11:55 -0800

    read encap Info for port 7123
    Summary: In diff D53256766 we added port 7123 for extended shiv monitoring
    
    Reviewed By: avasylev
    
    Differential Revision: D53570193
    
    fbshipit-source-id: baf4739aa3b4dbac9435d2cd74a0d24d12a1d349

--- katran/decap/tc_bpf/tc_decap_info.bpf.c
+++ katran/decap/tc_bpf/tc_decap_info.bpf.c
@@ -35,6 +35,7 @@
 
 #define DST_PORT_443 443
 #define DST_PORT_8080 8080
+#define DST_PORT_7123 7123
 
 __attribute__((__always_inline__)) static inline bool parse_inner_udp(
     void* data,
@@ -131,7 +132,8 @@ __attribute__((__always_inline__)) static inline int process_packet(
     }
     if ((pckt.flow.port16[1] == bpf_htons(GUE_DPORT)) &&
         ((inner_pckt.flow.port16[1] == bpf_htons(DST_PORT_443)) ||
-         (inner_pckt.flow.port16[1] == bpf_htons(DST_PORT_8080)))) {
+         (inner_pckt.flow.port16[1] == bpf_htons(DST_PORT_8080)) ||
+         (inner_pckt.flow.port16[1] == bpf_htons(DST_PORT_7123)))) {
       int ret = bpf_map_update_elem(
           &pkt_encap_info, &inner_pckt.flow, &pckt.flow, BPF_ANY);
       if (ret) {
