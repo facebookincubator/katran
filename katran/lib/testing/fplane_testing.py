b9444ad3e9147857ea9c0dbc3e356c288abd8591
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-12-09 12:18:43 -0800

    Add missing Pyre mode headers] [batch:16/1458] [shard:22/N] [B] [A] [B]
    Differential Revision: D66963590
    
    fbshipit-source-id: 2b1fc9574ce1b9637bfb0ae221de636df90ec336

--- katran/lib/testing/fplane_testing.py
+++ katran/lib/testing/fplane_testing.py
@@ -13,6 +13,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+# pyre-unsafe
+
 
 import argparse
 import time
