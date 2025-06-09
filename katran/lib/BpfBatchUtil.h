a2aaefee7a1b33b440717de815fbe5bb4afcf5ed
Ivan Morett <ivanmorett@meta.com>
2025-06-09 14:01:08 -0700

    get all bpf map through batch op
    Summary: I have a function to get specific keys from the map, adding one just to get all the map.
    
    Reviewed By: frankfeir
    
    Differential Revision: D76082045
    
    fbshipit-source-id: 23142e754ffeb9e5ca46e79fd0928834cc4d1d80

--- katran/lib/BpfBatchUtil.h
+++ katran/lib/BpfBatchUtil.h
@@ -78,6 +78,72 @@ class BpfBatchUtil {
   }
 
   template <typename KeyT, typename ValueT, typename HashT = std::hash<KeyT>>
+  static int bpfMapReadBatch(
+      int map_fd,
+      std::unordered_map<KeyT, std::vector<ValueT>>& foundMap,
+      std::uint32_t num_cpus = 1,
+      std::uint32_t batch_sz = 128) {
+    struct bpf_map_info mapInfo {};
+    auto err = BaseBpfAdapter::getBpfMapInfo(map_fd, &mapInfo);
+    if (err) {
+      LOG(ERROR) << "Error while retrieving map metadata for fd " << map_fd
+                 << " : " << folly::errnoStr(errno);
+      return err;
+    }
+
+    if (sizeof(KeyT) != mapInfo.key_size ||
+        sizeof(ValueT) != mapInfo.value_size) {
+      LOG(ERROR) << "caller used the wrong KeyT/ValueT for the given map";
+      return -EINVAL;
+    }
+
+    auto blob_size = num_cpus * sizeof(ValueT);
+    auto blob_buf_size = batch_sz * blob_size;
+    std::vector<KeyT> key_buf(batch_sz);
+    std::vector<char> val_blob_buf(blob_buf_size);
+    int lookupErr = 0;
+
+    void* inKey = nullptr; // NULL to start a new dump
+    void* nextKey = nullptr;
+
+    DECLARE_LIBBPF_OPTS(
+        bpf_map_batch_opts, opts, .elem_flags = 0, .flags = 0, );
+
+    while (!lookupErr) {
+      __u32 count = batch_sz; // in/out parameter for each call
+
+      lookupErr = bpf_map_lookup_batch(
+          map_fd,
+          &inKey,
+          &nextKey,
+          key_buf.data(),
+          val_blob_buf.data(),
+          &count,
+          &opts);
+
+      if (lookupErr && lookupErr != -ENOENT) {
+        // ENOENT is expected when we reach the end of the map
+        LOG(ERROR) << "Failed to perform batch lookup, errno = "
+                   << folly::errnoStr(-lookupErr);
+        return lookupErr;
+      }
+
+      for (uint32_t i = 0; i < count; ++i) {
+        const KeyT& k = key_buf[i];
+        if (foundMap.contains(k)) {
+          LOG(ERROR) << "Found duplicate key in returned bpf batch map";
+          continue;
+        }
+        auto* raw = reinterpret_cast<ValueT*>(&val_blob_buf[i * blob_size]);
+        std::vector<ValueT> values(raw, raw + num_cpus);
+        foundMap.emplace(k, std::move(values));
+      }
+      inKey = nextKey; // nextKey is the new inKey
+    }
+    return 0;
+  }
+
+  template <typename KeyT, typename ValueT, typename HashT = std::hash<KeyT>>
   static int bpfMapLookupBatch(
       int map_fd,
       const std::unordered_set<KeyT, HashT>& keys,
@@ -137,7 +203,7 @@ class BpfBatchUtil {
           continue;
         }
         if (foundMap.contains(k)) {
-          LOG(ERROR) << "Found duplicate key in returned bpf batch map: " << k;
+          LOG(ERROR) << "Found duplicate key in returned bpf batch map";
           continue;
         }
         auto* raw = reinterpret_cast<ValueT*>(&val_blob_buf[i * blob_size]);
