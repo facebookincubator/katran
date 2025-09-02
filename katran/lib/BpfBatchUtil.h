6ce7ff34c061e4826b2678992a1d2e6ec1e08782
Andrii Vasylevskyi <avas@meta.com>
2025-09-02 14:23:25 -0700

    Add "Copyright" to make oss integration checks happy
    Reviewed By: lima1756
    
    Differential Revision: D81514427
    
    fbshipit-source-id: a5305b95d266ee439dd396807a96c35c2c1f4462

--- katran/lib/BpfBatchUtil.h
+++ katran/lib/BpfBatchUtil.h
@@ -1,4 +1,4 @@
-/* (c) Meta Platforms, Inc. and affiliates
+/* Copyright (c) Meta Platforms, Inc. and affiliates
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
